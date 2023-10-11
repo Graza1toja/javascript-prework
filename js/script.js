af5faf6d7cd30a6a5910c9a6f966a158a86c85da
Grażyna Ciosek <gctatek@gmail.com>
2023-10-11 18:35:26 +0200

    Add Wstęp do gry

--- js/script.js
+++ js/script.js
@@ -7,13 +7,14 @@ let computerMove = 'nieznany ruch';
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
-else if(randomNumber == 2){
+if(randomNumber == 2){
     computerMove = 'papier';
 }
-else if(randomNumber == 3){
+if(randomNumber == 3){
     computerMove = 'nożyce';
 }
 printMessage('Mój ruch to: ' + computerMove);
+
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -24,10 +25,27 @@ let playerMove = 'nieznany ruch';
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
-else if(playerInput == '2'){
+if(playerInput == '2'){
     playerMove = 'papier';
 }
-else if(playerInput == '3'){
+if(playerInput == '3'){
     playerMove = 'nożyce';
 }
+if(playerInput > '3'){
+    playerMove = 'nieznany ruch';
+}
 printMessage('Twój ruch to: ' + playerMove);
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}
+if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}
+if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}
+else if(randomNumber == playerInput){
+    printMessage('Remis');
+}
